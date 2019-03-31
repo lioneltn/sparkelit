3a5d8a07e4488842d5a80c3038c18034f267de1c
ziedh77 <zied.hamza.1@esprit.tn>
2019-03-31 16:50:37 +0100

    Poste

---
+++ Poste/views/affichercommantaireduposte.php
@@ -0,0 +1,28 @@
+<?php 
+chdir(__DIR__);
+
+$note= new noteC();
+$liste = $note->affichercommantaire ($_POST['id']);
+$nb1=$note->nombrecommantaire($_POST['id']);
+$nb=$nb1['total'];
+if($nb==0)
+{
+	?>
+	<p style="padding-top: 10px;padding-bottom:  10px; font-size: 25px;word-wrap: break-word;">Pas de commantaires!</p>
+<?php
+}
+else
+{
+	foreach ( $liste as $row ) 
+
+	{
+		 ?>
+		 <div style=" border: solid 1.5px; border-radius: 20px; font-size: 20px ; word-wrap: break-word;padding: 10px; margin: 20px">
+		 	<p><?php echo $row['id_client'];?><strong> A dit:</strong> </p>
+		 	<?php echo $row['commantaire'];?> 
+		 </div>
+		 <?php 
+	}
+}
+
+ ?>
---
+++ Poste/views/afficherpostfrontTrieNoteASC.php
@@ -0,0 +1,237 @@
+
+
+<?php 
+
+chdir(__DIR__);
+
+include "../entities/poste.php";
+chdir(__DIR__);
+include "../core/postecore.php";
+chdir(__DIR__);
+include "../entities/note.php";
+chdir(__DIR__);
+include "../core/notecore.php";
+chdir(__DIR__);
+    $poste1C=new posteC();
+    $note1C= new noteC();
+    $liste=$poste1C->trieposteparnoteASC();
+
+
+?>
+
+<?PHP
+                foreach($liste as $row)
+                {
+                   $nb1=$note1C->nbnoteclientetposte($row['id_poste'],'4');
+                    $nb=$nb1['total'];
+                    if($nb!=0)
+                    {
+                     $listenote=$note1C->noteetcommentaireclientposte($row['id_poste'],"4");
+                     $note=$listenote['note'];
+                     $commantaire=$listenote['commantaire'];
+                    }
+                ?>
+                    <div class="form-group" style="width:600px; height: 950px;">
+                            <div class="entry-media">
+                                <a href="single.php">
+                                    <img style="width: 100%;height: 400px;" src="<?php echo '../'.$row['image'];?>" alt="Post">
+                                </a>
+                            </div><!-- End .entry-media -->
+
+                            <div class="entry-body" style="border:none">
+                                <div class="entry-date">
+                                    <?php
+                                        $date=explode("-",$row["date"]);
+                                        $jour=end($date);
+                                        switch($date[1])
+                                        {
+                                            case "01":
+                                                $mois="JAN";
+                                                break;
+                                            case "02":
+                                                $mois="FEV";
+                                                break;
+                                            case "03":
+                                                $mois="MAR";
+                                                break;
+                                            case "04":
+                                            $mois="AVR";
+                                                break;
+                                            case "05":
+                                                $mois="MAI";
+                                                break;
+                                            case "06":
+                                                $mois="JUN";
+                                                break;
+                                            case "07":
+                                                $mois="JUL";
+                                                break;
+
+                                            case "08":
+                                                $mois="AOU";
+                                                break;
+                                            case "09":
+                                                $mois="SEP";
+                                                break;
+                                            case "10":
+                                                $mois="OCT";
+                                                break;
+                                            case "11":
+                                                $mois="NOV";
+                                                break;
+                                            case "12":
+                                                $mois="DEC";
+                                                break;
+
+
+                                        }
+                                      ?>
+                                    <span class="day"><?PHP echo $jour; ?></span>
+                                    <span class="month"><?PHP echo $mois; ?></span>
+                                    <span class="day" style="background-color: white"><?php echo $row['note']; ?></span>
+                                </div><!-- End .entry-date -->
+
+                                <h2 class="entry-title">
+                                    <p style="text-decoration: underline;"><?PHP echo $row['titre']; ?></strang>
+                                </h2>
+
+                                <div class="entry-content">
+                                    <p style="width: 100% ;text-align:center ;word-wrap: break-word; height: 100px; font-size: 13px; "> <?PHP echo $row['description']; ?></p>
+
+                                   
+                                </div>
+                                    <form style="position: absolute;  padding-top: 20px; padding-bottom: 20px;" action="affichercommantairesporto.php" method="POST" >
+                                     <input type="hidden" name="id" value="<?php echo $row['id_poste'] ;?>">
+                                    <input type="submit" name="" value="Afficher les commantaires">
+                                </form>
+                                <br>
+                                <form action="../ajouterNote.php" method="POST">
+                                    <input type="hidden" name="id_poste" value="<?php echo $row['id_poste'] ;?>">
+                                      <input type="hidden" name="id_client" value="4">
+                                    <div class="rating" style="padding-top: 20px ">
+                                        <?php
+                                            if($nb!=0)
+                                            {
+
+                                        ?>
+                                            <input type="hidden" name="note" id="noteposte_<?php echo $row['id_poste'] ;?>" value="<?php echo $note; ?>" onchange="notechange(this.id)">
+                                            <?php 
+                                            if($note==1)//color: #303030; color:#f1c40f;
+
+                                            {
+                                            ?>
+                                                 <i style="color: #303030;" class="icon ion-md-star b1" id="b1_<?php echo $row['id_poste'] ;?>" onclick="note5(this.id)"></i>
+                                                  <i style="color: #303030;" class="icon ion-md-star b2" id="b2_<?php echo $row['id_poste'] ;?>" onclick="note4(this.id)"></i>
+                                                   <i style="color: #303030;" class="icon ion-md-star b3" id="b3_<?php echo $row['id_poste'] ;?>" onclick="note3(this.id)"></i>
+                                                    <i style="color: #303030;" class="icon ion-md-star b4" id="b4_<?php echo $row['id_poste'] ;?>" onclick="note2(this.id)"></i>
+                                                    <i style="color:#f1c40f;" class="icon ion-md-star b5" id="b5_<?php echo $row['id_poste'] ;?>"  onclick="note1(this.id)"></i>
+                                            <?php
+                                            }
+                                            if($note==2)
+                                            {
+                                            ?>
+                                                <i style="color: #303030;" class="icon ion-md-star b1" id="b1_<?php echo $row['id_poste'] ;?>" onclick="note5(this.id)"></i>
+                                                  <i style="color: #303030;" class="icon ion-md-star b2" id="b2_<?php echo $row['id_poste'] ;?>" onclick="note4(this.id)"></i>
+                                                   <i style="color: #303030;" class="icon ion-md-star b3" id="b3_<?php echo $row['id_poste'] ;?>" onclick="note3(this.id)"></i>
+                                                    <i style="color: #f1c40f;" class="icon ion-md-star b4" id="b4_<?php echo $row['id_poste'] ;?>" onclick="note2(this.id)"></i>
+                                                    <i style="color:#f1c40f;" class="icon ion-md-star b5" id="b5_<?php echo $row['id_poste'] ;?>"  onclick="note1(this.id)"></i>
+
+                                            <?php 
+                                            }
+                                            if($note==3)
+                                            {
+                                            ?>
+                                                <i style="color: #303030;" class="icon ion-md-star b1" id="b1_<?php echo $row['id_poste'] ;?>" onclick="note5(this.id)"></i>
+                                                  <i style="color: #303030;" class="icon ion-md-star b2" id="b2_<?php echo $row['id_poste'] ;?>" onclick="note4(this.id)"></i>
+                                                   <i style="color: #f1c40f;" class="icon ion-md-star b3" id="b3_<?php echo $row['id_poste'] ;?>" onclick="note3(this.id)"></i>
+                                                    <i style="color: #f1c40f;" class="icon ion-md-star b4" id="b4_<?php echo $row['id_poste'] ;?>" onclick="note2(this.id)"></i>
+                                                    <i style="color:#f1c40f;" class="icon ion-md-star b5" id="b5_<?php echo $row['id_poste'] ;?>"  onclick="note1(this.id)"></i>
+                                            <?php   
+                                            }
+                                            if($note==4)
+                                            {
+                                            ?>
+                                                <i style="color: #303030;" class="icon ion-md-star b1" id="b1_<?php echo $row['id_poste'] ;?>" onclick="note5(this.id)"></i>
+                                                  <i style="color: #f1c40f;" class="icon ion-md-star b2" id="b2_<?php echo $row['id_poste'] ;?>" onclick="note4(this.id)"></i>
+                                                   <i style="color: #f1c40f;" class="icon ion-md-star b3" id="b3_<?php echo $row['id_poste'] ;?>" onclick="note3(this.id)"></i>
+                                                    <i style="color: #f1c40f;" class="icon ion-md-star b4" id="b4_<?php echo $row['id_poste'] ;?>" onclick="note2(this.id)"></i>
+                                                    <i style="color:#f1c40f;" class="icon ion-md-star b5" id="b5_<?php echo $row['id_poste'] ;?>"  onclick="note1(this.id)"></i>
+
+                                            <?php 
+                                            }
+                                            if($note==5)
+                                            {
+
+
+                                            ?>
+                                            <i style="color: #f1c40f;" class="icon ion-md-star b1" id="b1_<?php echo $row['id_poste'] ;?>" onclick="note5(this.id)"></i>
+                                                  <i style="color: #f1c40f;" class="icon ion-md-star b2" id="b2_<?php echo $row['id_poste'] ;?>" onclick="note4(this.id)"></i>
+                                                   <i style="color: #f1c40f;" class="icon ion-md-star b3" id="b3_<?php echo $row['id_poste'] ;?>" onclick="note3(this.id)"></i>
+                                                    <i style="color: #f1c40f;" class="icon ion-md-star b4" id="b4_<?php echo $row['id_poste'] ;?>" onclick="note2(this.id)"></i>
+                                                    <i style="color:#f1c40f;" class="icon ion-md-star b5" id="b5_<?php echo $row['id_poste'] ;?>"  onclick="note1(this.id)"></i>
+                                            <?php
+                                            }
+                                            ?>
+
+                                            
+                                            
+
+    
+                                          
+
+
+                                            <?php
+                                            }
+                                            else
+                                            {
+                                            ?>
+                                                <input type="hidden" name="note" id="noteposte_<?php echo $row['id_poste'] ;?>" onchange="notechange(this.id)">
+                                                <i  class="icon ion-md-star b1" id="b1_<?php echo $row['id_poste'] ;?>" onclick="note5(this.id)"></i>
+                                                      <i  class="icon ion-md-star b2" id="b2_<?php echo $row['id_poste'] ;?>" onclick="note4(this.id)"></i>
+                                                       <i  class="icon ion-md-star b3" id="b3_<?php echo $row['id_poste'] ;?>" onclick="note3(this.id)"></i>
+                                                        <i  class="icon ion-md-star b4" id="b4_<?php echo $row['id_poste'] ;?>" onclick="note2(this.id)"></i>
+                                                        <i  class="icon ion-md-star b5" id="b5_<?php echo $row['id_poste'] ;?>"  onclick="note1(this.id)"></i>
+                                            <?php
+                                            }
+                                            ?>
+                                        
+                                        
+                                     
+                                        
+                                      
+                                    </div>
+                                     <div class="form-group">
+                                                <label>Comment</label>
+                                                <div class="form-group">
+                                                <?php
+                                                if($nb!=0)
+                                                {
+                                                ?>
+                                                    <textarea style="max-width: 10000px;width: 100%;" name="commentaire" class="form-control" ><?php echo $commantaire; ?></textarea>
+                                                <?php
+                                                }
+                                                else
+                                                {
+                                                ?>
+                                                 <textarea style="max-width: 10000px;width: 100%;" name="commentaire" class="form-control" ></textarea>
+                                                 <?php 
+                                                 } 
+                                                 ?>
+                                                </div>  
+                                                 <input type="submit" name="" class="btn btn-primary" value="Envoyer">
+                                            </div><!-- End .form-group --><!-- End .entry-content -->
+                                           
+                                            
+
+                                </form>
+                            
+
+                               
+                            </div><!-- End .entry-body -->
+
+                                       
+                        </div><!-- End .entry -->
+
+                    <?php 
+                    }
+                    ?>
