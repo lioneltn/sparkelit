928b9bbf79a9c2301bf7123daf04c79307d6e291
Lionel Tunisiano <lioneltunisianorevo@gmail.com>
2019-04-27 19:15:32 +0200

    Fixed merging conflict

--- views/backend/js/region-chart.js
+++ views/backend/js/region-chart.js
@@ -2,37 +2,53 @@ $(document).ready(function(){
   $.ajax({
     url: "../../json_country.php",
     method: "GET",
-    success: function(data) {
-      console.log(data);
-      var country = [];
-      var count = [];
+    success: function(data1) {
+      
+      let country = [];
+      let count = [];
+     
 
-      for(var i in data) {
-        country.push("Country " + data[i].country);
-        count.push(data[i].count);
+      for(var i in data1) {
+        country.push(data1[i].country);
+        count.push(data1[i].count);
       }
 
     // Region Charts Starts
 
 google.charts.load('current', {
-  'packages': ['geochart'],
+  'packages': ['geochart','bar', 'corechart', 'table'],
   // Note: you will need to get a mapsApiKey for your project.
   // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
   'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
 });
-google.charts.setOnLoadCallback(drawRegionsMap);
 
-function drawRegionsMap() {
-  var data = google.visualization.arrayToDataTable([
-    ['Country', 'Popularity'],
-    ['Germany', 200],
-    ['United States', 300],
-    ['Brazil', 400],
-    ['Canada', 500],
-    ['France', 600],
-    ['RU', 700],
-    ['TN', 700]
-  ]);
+
+      var timeout;
+
+ $(document).ready(function(){
+       timeout = setInterval(function () {
+          if (google.visualization != undefined) {
+             drawRegionsMap(country,count);
+             clearInterval(timeout);
+          }
+       }, 300);
+    });
+console.log(country);
+console.log(count);
+console.log(Array.isArray(country));
+function drawRegionsMap(country,count) {
+  
+  var data2 = new google.visualization.DataTable();
+  data2.addColumn('string', 'Country');
+  data2.addColumn('string', 'Number of visits');
+ 
+  for (var i = 0; i < country.length; ++i) {
+     
+
+    data2.addRow([country[i],count[i]]);
+}
+
+  
 
   var options = {
     colorAxis: {
@@ -40,7 +56,6 @@ function drawRegionsMap() {
     }
   };
   var chart = new google.visualization.GeoChart(document.getElementById('regions-chart'));
-<<<<<<< HEAD
   var chart_div = document.getElementById('regions-chart');
 
 
@@ -53,19 +68,7 @@ function drawRegionsMap() {
   document.getElementById('hidden_html').value = pdf_container;
 
       });
-||||||| merged common ancestors
-  var chart_div = document.getElementById('regions-chart');
 
-
-  // Wait for the chart to finish drawing before calling the getImageURI() method.
-      google.visualization.events.addListener(chart, 'ready', function () {
-        chart_div.innerHTML = '<img src="' + chart.getImageURI() + '">';
-        console.log(chart_div.innerHTML);
-      });
-=======
->>>>>>> 4c6e22f693ac17169d2bfa9352fd9f4344d7ba6f
-
-<<<<<<< HEAD
   
 
 
@@ -76,11 +79,6 @@ function drawRegionsMap() {
   
 
 
-||||||| merged common ancestors
-  chart.draw(data2, options);
-=======
-  chart.draw(data, options);
->>>>>>> 4c6e22f693ac17169d2bfa9352fd9f4344d7ba6f
 }
     },
     error: function(data) {
